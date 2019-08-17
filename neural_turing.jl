d02eb239c391de35d2bfd943b980a512306960e9
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2019-08-17 16:56:48 +0300

    code imp & cleanup

--- neural_turing.jl
+++ neural_turing.jl
@@ -1,16 +1,6 @@
 using AutoGrad: Param, @diff, value, grad
 using Knet: relu, sigm, tanh, softmax
 
-normalize(arr) =
-begin
-    norm = sqrt(sum(arr .^2))
-    if norm != 0
-        arr ./ norm
-    else
-        arr
-    end
-end
-
 
 
 in_size       = 12
@@ -19,6 +9,21 @@ out_size      = 12
 
 memory_size   = 128
 location_size = 32
+
+
+Layer_Type    = :FeedForward
+
+
+
+normalize(arr) = # why cant u normalize a 2d array LinearAlgebra
+begin
+    norm = sqrt(sum(arr .^2))
+    if norm != 0
+        arr ./ norm
+    else
+        arr
+    end
+end
 
 
 mutable struct Recurrent
@@ -50,28 +55,50 @@ begin
     focus  = sigm.(in * layer.wf1 + layer.state * layer.wf2 + layer.bf)
     keep   = sigm.(in * layer.wk1 + layer.state * layer.wk2 + layer.bk)
     interm = tanh.(in * layer.wi  + layer.state .* focus    + layer.bi)
+    layer.state = Param(keep .* interm + (1 .- keep) .* layer.state)
 
 layer.state = Param(keep .* interm + (1 .- keep) .* layer.state)
 end
 
 
+mutable struct FeedForward
+    w::Param
+    b::Param
+
+FeedForward(in_size, layer_size) = new(
+    Param(randn(in_size, layer_size)),
+    Param(zeros(1, layer_size)),
+)
+end
+
+(layer::FeedForward)(in) =
+begin
+
+tanh.(in * layer.w + layer.b)
+end
+
+
+
+Layer = eval(Layer_Type)
+
+
 mutable struct Processor
-    layer_1::Recurrent
-    layer_2::Recurrent
-    key_creator::Recurrent
-    importance_creator::Recurrent
+    layer_in::Layer
+    layer_out::Layer
+    key_creator::Layer
+    importance_creator::Layer
 
 Processor(in_size, hidden_size, out_size, location_size) = new(
-    Recurrent(in_size+location_size, hidden_size),
-    Recurrent(hidden_size          , out_size),
-    Recurrent(out_size             , location_size),
-    Recurrent(out_size             , 1),
+    Layer(in_size+location_size, hidden_size),
+    Layer(hidden_size          , out_size),
+    Layer(out_size             , location_size),
+    Layer(out_size             , 1),
 )
 end
 
 (processor::Processor)(input, memory_attended) =
 begin
-    output = processor.layer_2(processor.layer_1(hcat(input, memory_attended)))
+    output = processor.layer_out(processor.layer_in(hcat(input, memory_attended)))
     key = processor.key_creator(output)
     importance = (processor.importance_creator(output).+1)./2
 
@@ -79,10 +106,10 @@ output, key, importance
 end
 
 mutable struct Reader
-    read_focus::Recurrent
+    read_focus::Layer
 
 Reader(in_size, location_size) = new(
-    Recurrent(in_size, memory_size)
+    Layer(in_size, memory_size)
 )
 end
 
@@ -95,10 +122,10 @@ memory_attended
 end
 
 mutable struct Writer
-    memory_creator::Recurrent
+    memory_creator::Layer
 
 Writer(out_size, location_size) = new(
-    Recurrent(out_size, location_size),
+    Layer(out_size, location_size),
 )
 end
 
@@ -136,11 +163,9 @@ begin
 output, new_memory
 end
 
-
-propogate_time_series(sequence, model; memory=nothing) =
+propogate_timeseries(sequence, model; memory=nothing) =
 begin
-    # memory == nothing ? [zeros(1, location_size) for _ in 1:memory_size] : ()
-    memory = [zeros(1, location_size) for _ in 1:memory_size]
+    memory == nothing ? memory = [zeros(1, location_size) for _ in 1:memory_size] : ()
 
     response = []
     for timestep in sequence
@@ -148,11 +173,13 @@ begin
         push!(response, output)
     end
 
-    for component_name in fieldnames(typeof(model))
-        component = getfield(model, component_name)
-        for layer_name in fieldnames(typeof(component))
-            layer = getfield(component, layer_name)
-            layer.state = Param(zeros(size(layer.state)))
+    if Layer_Type == "Recurrent"
+        for component_name in fieldnames(typeof(model))
+            component = getfield(model, component_name)
+            for layer_name in fieldnames(typeof(component))
+                layer = getfield(component, layer_name)
+                layer.state = Param(zeros(size(layer.state)))
+            end
         end
     end
 
@@ -161,17 +188,20 @@ end
 
 
 
+
+
 ##TESTS
 
-# model = Model(in_size, hidden_size, out_size, memory_size, location_size)
-# memory = [zeros(1, location_size) for _ in 1:memory_size]
-#
-# sample_input = randn(1, in_size)
-# sample_output = randn(1, out_size)
-#
-# output, new_memory = model(sample_input, memory)
+model = Model(in_size, hidden_size, out_size, memory_size, location_size)
+memory = [zeros(1, location_size) for _ in 1:memory_size]
+
+sample_input = randn(1, in_size)
+sample_output = randn(1, out_size)
+
+output, new_memory = model(sample_input, memory)
 
 ##TESTS2
 
 sample_timeserie = [randn(1, in_size) for _ in 1:10]
-response = propogate_time_series(sample_timeserie, model)
+
+response = propogate_timeseries(sample_timeserie, model)
